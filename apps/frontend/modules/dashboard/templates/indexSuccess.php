5651a698daf18579a72d321edb9b5933c2a50775
Johann Reinke <johann.reinke@gmail.com>
2011-02-07 15:17:35 +0100

    Mise à jour du tableau de bord quand pas de graphique à afficher

--- apps/frontend/modules/dashboard/templates/indexSuccess.php
+++ apps/frontend/modules/dashboard/templates/indexSuccess.php
@@ -14,12 +14,15 @@
     </ul>
   <?php endif; ?>
 </div>
-<?php if (isset($report_by_period)): ?>
-  <div class="span-14 last">
-    <?php if (! empty($report_by_period)): ?>
-      <div id="chart1"></div>
-      <div id="chart2"></div>
-      <script type="text/javascript">
+<div class="span-14 last">
+  <div id="chart1"></div>
+  <div id="chart2"></div>
+
+  <script type="text/javascript">
+    var chart1;
+    var chart2;
+    $(document).ready(function() {
+      <?php if ($total_time_spent): ?>
         line1 = [];
         categories = [];
         <?php foreach (array_slice($report_by_period->getRawValue(), -8, null, true) as $period => $row): ?>
@@ -28,70 +31,69 @@
           categories.push('<?php echo substr($period, 0, 4) . ' S' . ltrim(substr($period, 4, 2), '0'); ?>');
         <?php endforeach; ?>
         yticks = [<?php echo implode(', ', range(0, round($total_time_spent))) ?>];
-        var chart1;
-        var chart2;
-        $(document).ready(function() {
-           chart1 = new Highcharts.Chart({
-              chart: {
-                 renderTo: 'chart1',
-                 defaultSeriesType: 'line'
-              },
-              title: { text: '<?php echo __('Progress') ?>' },
-              xAxis: { categories: categories },
-              yAxis: { title: { text: '<?php echo __('Days') ?>' } },
-              plotOptions: {
-                 line: {
-                    dataLabels: { enabled: true },
-                    enableMouseTracking: false
-                 }
-              },
-              credits: { enabled: false },
-              series: [{
-                 name: '<?php echo __('Time spent') ?>',
-                 data: line1
-              }]
-           });
 
-          data = [];
-          <?php foreach ($report_by_user->getRawValue() as $row): ?>
-            data.push(['<?php echo $row['name'] ? $row['name'] : __('Non assigned') ?>', <?php echo round($row['time_allocated'] * 100 / $total_time_allocated, 1) ?>]);
-          <?php endforeach; ?>
-          chart2 = new Highcharts.Chart({
-            chart: {
-               renderTo: 'chart2',
-               plotBackgroundColor: null,
-               plotBorderWidth: null,
-               plotShadow: false
-            },
-            title: {
-               text: '<?php echo __('User assignment') ?>'
-            },
-            tooltip: {
-               formatter: function() {
-                  return '<b>'+ this.point.name +'</b>: '+ this.y +' %';
-               }
-            },
-            plotOptions: {
-               pie: {
-                  cursor: 'pointer',
-                  size: '50%',
-                  dataLabels: {
-                     enabled: true,
-                     formatter: function() {
-                        return '<b>'+ this.point.name +'</b>: '+ this.y +' %';
-                     }
-                  }
-               }
-            },
-            credits: { enabled: false },
-            series: [{
-               type: 'pie',
-               name: 'Browser share',
-               data: data
-            }]
-          });
+        chart1 = new Highcharts.Chart({
+          chart: {
+             renderTo: 'chart1',
+             defaultSeriesType: 'line'
+          },
+          title: { text: '<?php echo __('Progress') ?>' },
+          xAxis: { categories: categories },
+          yAxis: { title: { text: '<?php echo __('Days') ?>' } },
+          plotOptions: {
+             line: {
+                dataLabels: { enabled: true },
+                enableMouseTracking: false
+             }
+          },
+          credits: { enabled: false },
+          series: [{
+             name: '<?php echo __('Time spent') ?>',
+             data: line1
+          }]
         });
-      </script>
-    <?php endif; ?>
-  </div>
-<?php endif; ?>
+      <?php endif; ?>
+
+      <?php if ($total_time_allocated): ?>
+        data = [];
+        <?php foreach ($report_by_user->getRawValue() as $row): ?>
+          data.push(['<?php echo $row['name'] ? $row['name'] : __('Non assigned') ?>', <?php echo round($row['time_allocated'] * 100 / $total_time_allocated, 1) ?>]);
+        <?php endforeach; ?>
+        chart2 = new Highcharts.Chart({
+          chart: {
+             renderTo: 'chart2',
+             plotBackgroundColor: null,
+             plotBorderWidth: null,
+             plotShadow: false
+          },
+          title: {
+             text: '<?php echo __('User assignment') ?>'
+          },
+          tooltip: {
+             formatter: function() {
+                return '<b>'+ this.point.name +'</b>: '+ this.y +' %';
+             }
+          },
+          plotOptions: {
+             pie: {
+                cursor: 'pointer',
+                size: '50%',
+                dataLabels: {
+                   enabled: true,
+                   formatter: function() {
+                      return '<b>'+ this.point.name +'</b>: '+ this.y +' %';
+                   }
+                }
+             }
+          },
+          credits: { enabled: false },
+          series: [{
+             type: 'pie',
+             name: 'Browser share',
+             data: data
+          }]
+        });
+      <?php endif; ?>
+    });
+  </script>
+</div>

6b53d1b13ae86a8c9c23b7e4e1fa374561fcd9a6
Johann Reinke <johann.reinke@gmail.com>
2012-01-23 12:08:26 +0100

    Correction du tri par défaut des priorités

--- lib/model/doctrine/PriorityTable.class.php
+++ lib/model/doctrine/PriorityTable.class.php
@@ -3,9 +3,14 @@
 
 class PriorityTable extends Doctrine_Table
 {
-    
+
     public static function getInstance()
     {
         return Doctrine_Core::getTable('Priority');
+    }
+
+    public function construct()
+    {
+      $this->_options['orderBy'] = 'id asc';
     }
 }

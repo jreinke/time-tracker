226e7f7b4a6b4cf979670f2a5d97a3363fdb97b2
Johann Reinke <johann.reinke@gmail.com>
2012-01-23 11:08:50 +0100

    Possibilité de se connecter via LDAP

--- apps/frontend/lib/myUser.class.php
+++ apps/frontend/lib/myUser.class.php
@@ -6,6 +6,30 @@ class myUser extends sfGuardSecurityUser
 
   protected $currentInputsUser = null;
 
+  public static function authLDAP($username, $password)
+  {
+    $conf = sfConfig::get('app_sf_guard_plugin_ldap_params');
+    $host = $conf['host'];
+    $port = isset($conf['port']) ? $conf['port'] : null;
+    $ldapconn = ldap_connect($host, $port);
+    if ($ldapconn)
+    {
+      if (isset($conf['protocol']))
+      {
+        ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, $conf['protocol']);
+      }
+      $ldaprdn = sprintf($conf['base_dn'], $username);
+      $ldapbind = @ldap_bind($ldapconn, $ldaprdn, $password);
+      if ($ldapbind)
+      {
+        ldap_unbind($ldapconn);
+        return true;
+      }
+    }
+
+    return false;
+  }
+
   public function signIn($user, $remember = false, $con = null)
   {
     parent::signIn($user, $remember, $con);

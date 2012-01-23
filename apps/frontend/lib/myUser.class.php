4df30adf7e367c6bbd960141c748c18eafb24b18
Johann Reinke <johann.reinke@gmail.com>
2012-01-23 12:24:19 +0100

    Authentification LDAP générique pour toutes les applications

--- apps/frontend/lib/myUser.class.php
+++ apps/frontend/lib/myUser.class.php
@@ -1,34 +1,10 @@
 <?php
 
-class myUser extends sfGuardSecurityUser
+class myUser extends ttUser
 {
   protected $currentProject = null;
 
   protected $currentInputsUser = null;
-
-  public static function authLDAP($username, $password)
-  {
-    $conf = sfConfig::get('app_sf_guard_plugin_ldap_params');
-    $host = $conf['host'];
-    $port = isset($conf['port']) ? $conf['port'] : null;
-    $ldapconn = ldap_connect($host, $port);
-    if ($ldapconn)
-    {
-      if (isset($conf['protocol']))
-      {
-        ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, $conf['protocol']);
-      }
-      $ldaprdn = sprintf($conf['base_dn'], $username);
-      $ldapbind = @ldap_bind($ldapconn, $ldaprdn, $password);
-      if ($ldapbind)
-      {
-        ldap_unbind($ldapconn);
-        return true;
-      }
-    }
-
-    return false;
-  }
 
   public function signIn($user, $remember = false, $con = null)
   {
---
+++ lib/ttUser.php
@@ -0,0 +1,28 @@
+<?php
+
+class ttUser extends sfGuardSecurityUser
+{
+  public static function authLDAP($username, $password)
+  {
+      $conf = sfConfig::get('app_sf_guard_plugin_ldap_params');
+      $host = $conf['host'];
+      $port = isset($conf['port']) ? $conf['port'] : null;
+      $ldapconn = ldap_connect($host, $port);
+      if ($ldapconn)
+      {
+          if (isset($conf['protocol']))
+          {
+              ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, $conf['protocol']);
+          }
+          $ldaprdn = sprintf($conf['base_dn'], $username);
+          $ldapbind = @ldap_bind($ldapconn, $ldaprdn, $password);
+          if ($ldapbind)
+          {
+              ldap_unbind($ldapconn);
+              return true;
+          }
+      }
+
+      return false;
+  }
+}
